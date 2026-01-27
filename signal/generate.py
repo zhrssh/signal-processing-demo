fb85332519ca745e9d78dfe2b2fd41c5216fc0be
zhrssh <98146850+zhrssh@users.noreply.github.com>
2026-01-27 19:25:30 +0800

    Initial commit

---
+++ signal/generate.py
@@ -0,0 +1,10 @@
+"""
+Module for generating syntethic signals. For demo purposes only.
+"""
+
+
+def generate_signal():
+    """
+    Generates a signal based on specification
+    """
+    raise NotImplementedError
