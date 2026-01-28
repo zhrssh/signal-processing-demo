9cf51180e751139974e13e06baa568b1498d97b4
zhrssh <98146850+zhrssh@users.noreply.github.com>
2026-01-28 12:20:05 +0800

    Added docstrings to tests

--- tests/unit/test_generate.py
+++ tests/unit/test_generate.py
@@ -21,6 +21,9 @@ from src.generate import (
     (None, 1000.0, 1.0),
 ])
 def test_generate_signal_invalid_types(freq, fs, duration):
+    """
+    Test for generating signal with invalid types.
+    """
     with pytest.raises(TypeError):
         generate_signal(freq, fs, duration)
 
@@ -31,11 +34,17 @@ def test_generate_signal_invalid_types(freq, fs, duration):
     (60.0, 1000.0, -1.0),
 ])
 def test_generate_signal_invalid_values(freq, fs, duration):
+    """
+    Test for generating signal with invalid values.
+    """
     with pytest.raises(ValueError):
         generate_signal(freq, fs, duration)
 
 
 def test_generate_signal():
+    """
+    Test for generating signal with good values.
+    """
     # test settings
     freq = 60.0
     fs = 1000.0
@@ -63,6 +72,9 @@ def test_generate_signal():
     (None, 1000.0, 1.0, 42),
 ])
 def test_generate_gaussian_noise_invalid_types(size, mean, std, seed):
+    """
+    Test for generating gaussian noise with invalid types.
+    """
     with pytest.raises(TypeError):
         generate_gaussian_noise(size, mean, std, seed)
 
@@ -72,11 +84,17 @@ def test_generate_gaussian_noise_invalid_types(size, mean, std, seed):
     (100, 0.0, -1.0, 42)
 ])
 def test_generate_gaussian_noise_invalid_values(size, mean, std, seed):
+    """
+    Test for generating gaussian noise with invalid values.
+    """
     with pytest.raises(ValueError):
         generate_gaussian_noise(size, mean, std, seed)
 
 
 def test_generate_gaussian_noise():
+    """
+    Test for generating gaussian noise with good values.
+    """
     size = 1000
     noise = generate_gaussian_noise(size, mean=0.0, std=1.0)
 
